fff98ef5a3b73e5b6db88ed0aec3f5e751481a81
yellyoshua <usuario1529yoshua@hotmail.com>
2022-05-02 13:31:02 -0500

    extract the word "with"

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[39:-63]} with {str[:6]}")
+print(f"{str[39:-63]} {str[107:111]} {str[:6]}")
