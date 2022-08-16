e5c97f0f80e8e39eb6bbeeb9faf8c11fa266837a
yellyoshua <usuario1529yoshua@hotmail.com>
2022-08-15 20:07:29 -0500

    add javascript warm up project

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+
+function add(a, b) {
+  console.log(a + b);
+}
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,3 @@
+# 0x12. JavaScript - Warm up
+
+In this warm up, we will be learning about the JavaScript language.
