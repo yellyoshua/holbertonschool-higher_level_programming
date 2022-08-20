042b0496ce673494087f0cd151fb29006b49a8f9
yellyoshua <usuario1529yoshua@hotmail.com>
2022-08-20 15:27:11 -0500

    fix script that group by user id

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+const dict = require('./101-data.js').dict;
+const group_by_user_id = {};
+Object.keys(dict).forEach(value => {
+	const user_id = dict[value];
+	if (!group_by_user_id[user_id]) {
+		group_by_user_id[user_id] = [];
+	}
+	group_by_user_id[user_id].push(value);
+});
+console.log(group_by_user_id);
